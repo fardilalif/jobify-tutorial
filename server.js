d73317aa4ac2862efd98694b4dddccf8bdfbc930
fardilalif <muhdfardilalif@gmail.com>
2024-02-27 10:57:12 +0800

    fixed frontend to be programmatically built for production

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ cloudinary.config({
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -42,7 +42,7 @@ app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
